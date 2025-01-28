7bfb73330b48de78b74472e991d83e2353165d73
Àlex Ollé <alex.olle.93@gmail.com>
2025-01-28 15:42:55 +0100

    Add data

--- src/data/airfryer_times.ts
+++ src/data/airfryer_times.ts
@@ -2,6 +2,8 @@ import type { TemperatureElement } from "../types";
 
 export const AIRFRYER_TIMES: TemperatureElement[] = [
   { name: "Escalivada", time: 30, temperature: 200 },
+  { name: "Pebrot fregit", time: 14, temperature: 180 },
   { name: "Pebrots del padrón", time: 8, temperature: 180 },
+  { name: "Pollastre", time: 20, temperature: 180 },
   { name: "Verdures rostides", time: 26, temperature: 175 },
 ];
--- src/data/pot_times.ts
+++ src/data/pot_times.ts
@@ -1,7 +1,21 @@
 import type { IntensityElement } from "../types";
 
 export const POT_TIMES: IntensityElement[] = [
+  { name: "Bleda - fulla", time: 5, intensity: "Alta" },
+  { name: "Bròcoli", time: 5, intensity: "Alta" },
   { name: "Bròquil", time: 8, intensity: "Alta" },
+  { name: "Carbassa", time: 20, intensity: "Alta" },
+  { name: "Carbassó", time: 5, intensity: "Alta" },
+  { name: "Cigrons", time: 60, intensity: "Baixa" },
+  { name: "Col", time: 6, intensity: "Alta" },
   { name: "Coliflor", time: 10, intensity: "Alta" },
+  { name: "Espinacs", time: 8, intensity: "Alta" },
+  { name: "Llenties", time: 30, intensity: "Alta" },
+  { name: "Mongeta", time: 90, intensity: "Baixa" },
   { name: "Mongeta tendra", time: 15, intensity: "Alta" },
+  { name: "Pastanaga", time: 10, intensity: "Alta" },
+  { name: "Patata", time: 20, intensity: "Alta" },
+  { name: "Pèsols", time: 15, intensity: "Alta" },
+  { name: "Quinoa", time: 15, intensity: "Mitja" },
+  { name: "Soja", time: 120, intensity: "Alta" },
 ];
