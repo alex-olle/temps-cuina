ae545e76a63edf10a31d9f86da02443d566cc3b4
Alejandro Ollé <61025241+alex-olle@users.noreply.github.com>
2025-03-27 17:44:23 +0100

    Update airfryer_times.ts

--- src/data/airfryer_times.ts
+++ src/data/airfryer_times.ts
@@ -1,10 +1,11 @@
 import type { TemperatureElement } from "../types";
 
 export const AIRFRYER_TIMES: TemperatureElement[] = [
-  { name: "Escalivada", time: 30, temperature: 200 },
+  { name: "Escalivada", time: 40, temperature: 200 },
   { name: "Ou dur", time: 15, temperature: 150 },
   { name: "Patata petita", time: 30, temperature: 200 },
-  { name: "Patata gran", time: 40, temperature: 200 },
+  { name: "Patata mitjana", time: 40, temperature: 200 },
+  { name: "Patata gran", time: 50, temperature: 200 },
   { name: "Patates fregides (1a fase)", time: 20, temperature: 120 },
   { name: "Patates fregides (2a fase)", time: 10, temperature: 180 },
   { name: "Pebrot fregit", time: 14, temperature: 180 },
